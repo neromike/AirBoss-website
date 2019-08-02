f9d53f790e47b185018fe7e121092381dcab9093
Sebastian Pereanu <neromike@Waynes-MBP.home>
2019-08-02 18:16:25 -0400

    Update copy and images

--- services.php
+++ services.php
@@ -5,6 +5,9 @@
 .service_container {
   margin-bottom: 2em;
 }
+.service_img img {
+  width: 100%;
+}
 .service_link:hover {
   text-decoration: none;
   color: <?php echo $color_base_dark; ?>;
@@ -19,35 +22,69 @@
 
 	<?php
 	$services = array(
-	  array("Conceptual Design",
+	  array("Conceptual Design & Preliminary Engineering",
           "services_conceptual-design",
-          "<p>AirBoss works with our clients to define the overall conceptual design of the project. From there, the vehicle is laid out in CAD and tweaked and tugged until the basic criterion is met.  The aircraft is then modeled in three dimensional CAD with definitive mold lines.</p>",
-          "<p>Once the mold lines are determined, the detailed design begins.  The parts are designed and analyzed, AirBoss then documents the design in CAD for fabrication.</p>
-          <p>Using the latest in solid modeling software, the aircraft is 'built' in the computer first.  This resolves a host of issues such as aerodynamics, actuation of assemblies, interference, and part count and identification.  The ability to spin the aircraft and look from any angle, inside or out, is a tremendous advantage in design.</p>
-          <p>The generated computer output can be sent directly to the manufacturer in a variety of formats.  From there, CNC tooling will generated composite parts.  Individual part fabrication is simplified from the simplest part to the most complex machining.</p>"),
-	  array("Structural Analysis",
-          "services_structural-analysis",
-          "<p>AirBoss offers structural analysis services.  Utilizing powerful finite element analysis programs, parts can be analyzed as an individual or in complete systems.</p>",
-          "<p>Systems can be analyzed for stresses, deflections, modal frequencies, mode shapes, heat transfer, and a host of other things.  Event simulation can also be analyzed.  Using modal data, preliminary flutter speeds can be determined and correlated in future flight testing.</p>
-          <p>After every phase of engineering and analysis, a report is generated to cover the methods and results of the analysis.  Using the results, composite laminate schedules can be identified and specified on accompanying part drawings.  Every part we design is individually documented in CAD with a part number.  Parts are easily identified down to the nut and bolt.</p>"),
+          "<ul>
+           <li>Develop initial concepts for stated mission</li>
+           <li>Perform preliminary design review</li>
+           </ul>",
+          "<p>AirBoss Aviation works with our clients to first define the overall conceptual design of the project. We lay out the vehicle in Computer-aided Design (CAD) software and make continual adjustments until the basic criterion is met. With the latest in three-dimensional solid modeling software, the aircraft is then &quot;built&quot; in the computer. The ability to spin the aircraft and look from any angle, inside or out, is a tremendous advantage in conceptual design.</p>
+           <p>After the conceptual design is complete, we perform a Preliminary Design Review (PDR). During the PDR, we address technical objectives, feasibility, functional/performance requirements, preliminary mechanical/structural/thermal/aerodynamic analysis, software requirements, and parts/materials selection.</p>
+           <p>The completion of a successful PDR marks the end of the preliminary design phase of the project and the beginning of detailed engineering.</p>"),
+	  array("Detailed Engineering",
+          "services_detailed-engineering",
+          "<ul>
+           <li>Computational fluid dynamics (CFD)</li>
+           <li>Finite element analysis (FEA)</li>
+           <li>Thermal Analysis - thermal analysis</li>
+           <li>Structural and subsystem design</li>
+           <li>Subsonic/Transonic/Supersonic flutter analysis</li>
+           </ul>",
+          "<p>By designing in three-dimensional software, we can convert the conceptual design into a computational fluid dynamic (CFD) model. With leading experts in the field, we explore aerodynamic flow, Mach effects, and body interaction. We then combine CFD with finite element analysis to study flow and heating issues. Utilizing powerful finite element analysis programs, we analyze parts as individual components and complete systems.</p>
+           <p>The next step is the detailed structural and system design. We analyze systems for stresses, deflections, modal frequencies, mode shapes, heat transfer, event simulation, and a host of other factors. Using modal data, we determine preliminary flutter speeds to correlate in future flight testing.</p>
+           <p>After every phase of engineering and analysis, we generate a report to cover the methods and results of the analysis. Using the results, we identify and specify composite laminate schedules on accompanying part drawings. Every part we design is individually documented in CAD with a part number. Parts are easily identified down to the nut and bolt. We maintain all revision history through Product Data Management (PDM) software systems.</p>
+           <p>We can deliver supporting design documentation in a variety of formats. This enables us to send solid model information directly to a Computer Numerical Control (CNC) subcontractor to generate rapid prototype tools, parts, or models. We also integrate all vendor information into the design to keep an accurate bill of materials for the project and to produce detailed assembly drawings for installation.</p>"),
+    array("Prototype Construction",
+          "services_prototype-construction",
+          "<ul>
+           <li>Composite component fabrication</li>
+           <li>Prototype construction</li>
+           <li>Prototype or production tooling</li>
+           <li>Prototype or production jigs and fixtures (manual or automated)</li>
+           <li>Production Set-up</li>
+           </ul>",
+          "<p>We can manage and build your prototype. We have partnered with some of the best talent when it comes to building a proof of concept, flight worthy aircraft. Composites Universal Group, located in Scappoose, Oregon has worked closely with us to bring many projects to reality. We have also worked closely with Phoenix Technology Works, and use their Computer Numerical Control (CNC) plug and mold making capabilities.</p>
+           <p>Other specialists are brought in during the fabrication of the prototype. AirBoss oversees the entire systems integration into the vehicle as the systems are developed and installed. We ensure that at the end of prototype construction, all supporting documentation and drawings reflect the numerous changes that come with the first prototype fabrication.</p>
+           <p>When the time comes, we will show up at the job site with our staff of engineers and support.  We can bring fabricators, engineers, outside specialists, and even test pilots. On-site program management is critical for the success of the program.</p>"),
+    array("Flight Testing",
+          "services_flight-testing",
+          "<ul>
+           <li>Flight or system tests</li>
+           <li>Test plan development</li>
+           </ul>",
+          "<p>AirBoss works closely with Skip Holm, the highest decorated combat pilot in the modern jet era and five time Reno Unlimited Gold Race winner, for some of the test flying needs. Skip is a former test pilot for Lockheed and performed secret tests for the U-2 and the Have Blue (F-117) projects, as well as the SR-71...and many more he won't talk about.</p>
+           <p>AirBoss also works closely with Len Fox. Len has flown nearly all of our projects to date. Len received the Spirit of Flight award at Oshkosh 2005, a yearly award presented by the Society of Experimental Test Pilots.</p>
+           <p>Both pilots provide an extensive and well thought out flight test program to any project, keeping safety paramount.</p>"),
 	  array("Project Management",
           "services_project-management",
-          "<p>Taking a design from conception to completion is a very complex process. Many factors must be taken into account and scheduled. AirBoss handles this task early on with task management and critical path analysis. We can chart out the timelines for design, construction, and subsequent production.</p>",
-          "<p>These tasks are usually associated with financial outlays as a function of project development. AirBoss can identify, categorize, and subsequently schedule the financial requirements as a function of timeline development. We can identify development costs for both long term and short term. We can identify the ramp-up costs associated with subsequent production. We can offer a cash flow analysis and income analysis for both prototype and production. Knowing beforehand is the strongest form of knowledge.</p>
-          <p>Once the timelines and financial outlays are identified, critical paths are determined. These paths, as the name implies, are the driving forces of the project. If the critical path is violated, the project schedule suffers. AirBoss identifies the critical paths and through proper project management, ensures that the schedule is met in a timely fashion.</p>
-          <p>AirBoss strives to be a single source where dreams can become reality. We are proud of our services and ensure that our clients are too. Together, we can lay out the best course that will ensure the successful completion of the project.</p>"),
-	  array("Prototype Construction",
-          "services_prototype-construction",
-          "<p>AirBoss can manage and build your prototype.  We have partnered with some of the best talent when it comes to building a proof of concept, flight worthy aircraft.  Composites Universal Group, located in Scappoose, Oregon has worked closely with us to bring many projects to reality.</p>",
-          "<p>AirBoss has also worked closely with Phoenix Technology Works, and use their CNC plug and mold making capabilities.</p>
-          <p>Other specialists are brought in during the fabrication of the prototype.  AirBoss can oversee the entire systems integration into the vehicle as the systems are developed and installed.</p>
-          <p>AirBoss can also ensure that at the end of prototype construction, all supporting documentation and drawings reflect the numerous changes that come with the first prototype fabrication.</p>"),
-	  array("Flight Testing",
-          "services_flight-testing",
-          "<p>AirBoss works closely with Skip Holm, the highest decorated combat pilot in the modern jet era as well as a five time Reno Unlimited Gold Race winner, for some of the test flying needs.</p>",
-          "<p>Skip is a former test pilot for Lockheed and performed secret tests for the U-2 and the Have Blue (F-117) projects as well as the SR-71...and many more he won't talk about.</p>
-          <p>AirBoss also works closely with Len Fox.  Len has flown nearly all of our projects to date.  Len recieved the Spirit of Flight award at Oshkosh 2005, a yearly award presented by the Society of Experimental Test Pilots.</p>
-          <p>Both pilots provide an extensive and well though out flight test program to any project, keeping safety paramount.</p>")
+          "<ul>
+           <li>Project management</li>
+           <li>R&D program planning</li>
+           <li>Critical path development</li>
+           <li>Production Assessment</li>
+           <li>Part vs. Production Compatibility</li>
+           </ul>",
+          "<p>Taking a design from conception to completion is a very complex process. Many factors must be taken into account and scheduled. AirBoss handles this task early on with task management and critical path analysis. We can chart out the timelines for design, construction, and subsequent production.</p>
+           <p>We assist our clients in financial planning. Our team will:
+           <ul>
+           <li>Identify, categorize, and schedule the financial requirements</li>
+           <li>Identify development costs for both long term and short term</li>
+           <li>Identify the ramp-up costs associated with subsequent production</li>
+           <li>Offer a cash flow analysis and income analysis for both prototype and production.</li>
+           </ul>
+           </p>
+           <p>Once the timelines and financial outlays are identified, critical paths are determined. These paths, as the name implies, are the driving forces of the project. If the critical path is violated, the project schedule suffers. AirBoss identifies the critical paths and through proper project management, ensures that the schedule is met in a timely fashion.</p>
+           <p>AirBoss strives to be a single source where dreams can become reality. We are proud of our services and ensure that our clients are too. Together, we can lay out the best course that will ensure the successful completion of the project.</p>")
 	);
 	?>
   <script>
@@ -58,8 +95,8 @@
     <div class="d-none d-md-block col-md-1"></div>
     <div class="service_container col-12 col-md-10">
 			<div class="row">
-				<div class="service_img col-12 col-md-3">
-          <img src="img/services/<?php echo $service[1]; ?>.jpg" />
+				<div class="service_img col-12 col-md-3 align-self-center">
+          <img src="img/service/<?php echo $service[1]; ?>.jpg" />
 				</div>
 				<div class="col-12 col-md-9 text-left service_link" id="<?php echo $service[1]; ?>">
           <h3 class="service_name"><?php echo $service[0]; ?></h3>
