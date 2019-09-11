3e77eaa961674fa300e78f97755a9e7589c84b18
Sebastian Pereanu <neromike@Waynes-MBP.fios-router.home>
2019-09-11 13:23:13 -0400

    Change where contact form email is sent

--- send_mail.php
+++ send_mail.php
@@ -5,7 +5,7 @@ $email = isset($_REQUEST['email'])?$_REQUEST['email']:"";
 $subject = isset($_REQUEST['subject'])?$_REQUEST['subject']:"";
 $message = isset($_REQUEST['message'])?$_REQUEST['message']:"";
 
-mail("airbossaviationgroup@gmail.com",
+mail("info@airbossaviation.com",
      "AirBoss Website Contact Message",
      "You have received a message from the contact form on the AirBoss website.\nName : " . $name . "\nEmail : " . $email . "\nSubject : " . $subject . "\nMessage: " . $message . "\n\nThe message was sent from IP address " . $_SERVER['REMOTE_ADDR']);
 
